b4cdf172d53780782ad3195352d7d17606a499c8
kevin <you@example.com>
2023-04-24 21:37:51 -0500

    se agregan detalles a atributos

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,26 @@ class NinjaCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
-              child: CircleAvatar(backgroundImage: AssetImage('assets/images/rere.jpg'),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/rere.jpg'),
                 radius: 40.0,
               ),
             ),
             Divider(
-              height:90.0,
+              height: 90.0,
               color: Colors.grey[800],
             ),
             Text(
               'ALIAS',
               style: TextStyle(
+                textBaseline: TextBaseline.ideographic,
                 fontStyle: FontStyle.italic,
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
             ),
             SizedBox(
-              height: 10.0,
+              height: 8.0,
             ),
             Text(
               'CAZADOR',
@@ -58,7 +60,7 @@ class NinjaCard extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 10.0,
+              height: 8.0,
             ),
             Text(
               'NV: 25',
@@ -68,14 +70,97 @@ class NinjaCard extends StatelessWidget {
                   fontSize: 24.0,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0),
+            SizedBox(height: 17.0),
+            Text(
+              'ATRIBUTOS',
+              style: TextStyle(
+                textBaseline: TextBaseline.ideographic,
+                fontStyle: FontStyle.italic,
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Row(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.red[900],
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(00.1),
+                  child: Text(
+                    'Power: 200',
+                    style: TextStyle(
+                      // letterSpacing: 2.0,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Icon(
+                    Icons.security,
+                    color: Colors.brown[600],
+                  ),
+                ),
+                //SizedBox(height: 12.0),
+                Padding(
+                  padding: const EdgeInsets.all(00.1),
+                  child: Text(
+                    'Endurance: 400',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.green[500],
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(00.1),
+                  child: Text(
+                    'life: 900',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 17.0),
+            Text(
+              'CONTACTO',
+              style: TextStyle(
+                textBaseline: TextBaseline.ideographic,
+                fontStyle: FontStyle.italic,
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
             Row(
               children: <Widget>[
                 Icon(
                   Icons.email,
                   color: Colors.grey[400],
                 ),
-                SizedBox(height: 10.0),
+                SizedBox(height: 8.0),
                 Text(
                   'elcazador@gmail.com',
                   style: TextStyle(
@@ -85,46 +170,6 @@ class NinjaCard extends StatelessWidget {
               ],
             ),
             SizedBox(height: 17.0),
-            Row(
-
-              children: <Widget>[
-
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.red[900],
-                ),
-                SizedBox(height: 12.0),
-                Text(
-                  'Power: 200',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                  ),
-                ),
-                Icon(
-                  Icons.security,
-                  color: Colors.brown[600],
-                ),
-                SizedBox(height: 12.0),
-                Text(
-                  'Resis: 400',
-                  style: TextStyle(
-
-                    color: Colors.grey[400],
-                  ),
-                ),
-                Icon(
-                  Icons.favorite,
-                  color: Colors.green[500],
-                ),
-                SizedBox(height: 12.0),
-                Text(
-                  'life: 900',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                  ),
-                ),
-              ],
-            ),
           ],
         ),
       ),
